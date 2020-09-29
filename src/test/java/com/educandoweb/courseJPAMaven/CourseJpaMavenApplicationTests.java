6a953e51c019b4177b8983fd53508ab307aa6c90
Tiago Almeida <tiago.almeida.poa@gmail.com>
2020-09-29 08:41:09 -0300

    projeto courseJPAMaven criado

---
+++ src/test/java/com/educandoweb/courseJPAMaven/CourseJpaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseJPAMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJpaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
