d37168de0097e3556c2f20465ae2b1c5d70a22f4
Tiago Almeida <tiago.almeida.poa@gmail.com>
2020-10-01 11:55:53 -0300

    Product

---
+++ src/main/java/com/educandoweb/courseJPAMaven/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.courseJPAMaven.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.courseJPAMaven.entities.Product;
+import com.educandoweb.courseJPAMaven.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
