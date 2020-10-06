fe209e071a08a4a314c5fd226baff00741a31a4f
Tiago Almeida <tiago.almeida.poa@gmail.com>
2020-10-05 22:03:57 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/courseJPAMaven/entities/Order.java
+++ src/main/java/com/educandoweb/courseJPAMaven/entities/Order.java
@@ -41,7 +41,7 @@ public class Order implements Serializable {
 	private Set<OrderItem>  items = new HashSet<>();
 	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
+	private Payment payment;	
 
 	public Order() {
 
@@ -100,6 +100,14 @@ public class Order implements Serializable {
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubtotal();
+		}
+		return sum;
 	}
 
 	@Override
